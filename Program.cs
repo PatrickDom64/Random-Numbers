21601cc49c4c1d3880f08560b9be1c77c660da28
Patrick Dom <44453042+PatrickDom64@users.noreply.github.com>
2023-03-04 22:25:19 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace RandomNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //declaração do objeto (declarete a new object)
+        Random random = new Random();
+
+        int number = random.Next(1,30);
+
+         if(number == 30)
+            {
+                Console.WriteLine("Goal!!!\n");
+                Console.WriteLine("You take the luck number: " + number);
+            }
+         else
+            {
+                Console.WriteLine("You lost!!! Try again\n" + "Your nabember is:" + number);
+            }
+        }
+    }
+}
